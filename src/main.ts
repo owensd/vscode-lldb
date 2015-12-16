6d61f242df3436a901748489b3493260f4c7ec64
David Owens II <david@owensd.io>
2015-12-16 00:05:13 -0800

    Debug console rewired.
    The debug console is rewired and working, but it’s a big clunky at the
    moment.

--- src/main.ts
+++ src/main.ts
@@ -29,104 +29,86 @@ interface DebugBreakpoint {
 	line: number;
 }
 
-class LLDBDebugger {
-	debugger: Promise<ChildProcess>;
-	lldb: ChildProcess;
-	session: SwiftDebugSession;
+class LLDB {
+	debugger: ChildProcess;
 	
 	onstdout: (str: string) => void;
 	onstderr: (str: string) => void;
 	
-	public constructor(session: SwiftDebugSession) {
+	public constructor(
+		onstdout: (str: string) => void,
+		onstderr: (str: string) => void)
+	{
 		this.debugger = null;
-		this.session = session;
+		this.onstdout = onstdout;
+		this.onstderr = onstderr;
 	}
 	
-	public connect(program: string): Promise<ChildProcess> {
-		console.log("LLDBDebugger: connect");
+	public connect(program: string): Promise<boolean> {
 		let self = this;
-		this.debugger = new Promise((resolve, reject) => {
+
+		return new Promise((resolve, reject) => {
 			let toolpath = getToolPath("lldb")
 			if (toolpath == null) {
-				return reject("Cannot find LLDB debugger.");
+				return reject("Cannot find LLDB debugger on the system PATH.");
 			}
 			console.log(`Using LLDB : ${toolpath}`)
 			
 			let lldb = spawn("lldb", [program]);
 
-			lldb.stdout.on("data", (str: string) => {
-				console.log("stdout: " + str);
-				self.session.sendEvent(new OutputEvent(str + "", 'stdout'));
+			lldb.stdout.on("data", (bytes: Uint8Array) => {
+				let str = (bytes + "");
+				console.log(str);
+				
+				if (!str.startsWith("(lldb)")) {
+					if (self.onstdout) {
+						self.onstdout(str);
+					}
+				}
 			});
-			lldb.stderr.on("data", (str: string) => {
-				console.error("stderr: " + str);
-				self.session.sendEvent(new OutputEvent(str + "", 'stdout'));
+			
+			lldb.stderr.on("data", (bytes: Uint8Array) => {
+				let str = (bytes + "");
+				console.log(str);
+				
+				if (!str.startsWith("(lldb)")) {
+					if (self.onstderr) {
+						self.onstderr(str);
+					}
+				}
 			});
+			
 			lldb.on("close", (code: number) => {
-				console.error("Process exiting with code: " + code);
+				console.log(`"Process exiting with code: ${code}`);
 				reject(code);
 			});
 			lldb.on("error", (err) => {
-				console.error("error: " + err);
-				self.session.sendEvent(new OutputEvent(err + "", 'stdout'));
+				console.log(err + "");
 				reject(err);
 			});
 			
-			// this is pretty hacky...
-			this.lldb = lldb;
-			resolve(lldb);
-		});
-		
-		return this.debugger;
-	}
-	
-	public setBreakpoint(filename: string, linenumber: number): Promise<boolean> {
-		console.log("LLDBDebugger: setBreakpoint");
-		return new Promise((resolve, reject) => {
+			self.debugger = lldb;
 			resolve(true);
 		});
 	}
-	
-	public setBreakpoints(filename: string, linenumbers: number[]): Promise<boolean> {
-		// console.log("LLDBDebugger: setBreakpoint");
-		// return new Promise((resolve, reject) => {
-		// 	for (var idx in linenumbers) {
-		// 		this.lldb.stdin.write(`breakpoint set -f ${filename} -l ${linenumbers[idx]}\n`);
-		// 	}
-		// 	this.lldb.stdin.write("r\n");
-		// 	resolve(true);
-		// });
-	}
 
-
-	public removeBreakpoint(id: number): Promise<boolean> {
-		console.log("LLDBDebugger: removeBreakpoint");
-		return new Promise((resolve, reject) => {
-			resolve(false);
-		});
-	}
-	
-	public removeAllBreakpoints(): Promise<boolean> {
-		console.log("LLDBDebugger: removeAllBreakpoints");
-		return new Promise((resolve, reject) => {
-			resolve(false);
-		});
-	}
-	
 	public evaluate(expression: string): Promise<boolean> {
-		return new Promise((resolve, reject) => {
-			this.lldb.stdin.write(`${expression}\n`);
-			resolve(true);
-		});
+		return this.doCommand(expression);
 	}
 
+	private doCommand(command: string): Promise<boolean> {
+		let self = this;
+		return new Promise((resolve, reject) => {
+			self.debugger.stdin.write(`${command}\n`, () => resolve(true));
+		});
+	}
 }
 
 
 class SwiftDebugSession extends DebugSession {
 	private static MAIN_THREAD_ID = 0;
 	
-	private lldb: LLDBDebugger;
+	private lldb: LLDB;
 	private breakpoints: Map<string, DebugProtocol.Breakpoint[]>;
 	
 	private program: string;
@@ -134,7 +116,10 @@ class SwiftDebugSession extends DebugSession {
 	
 	public constructor(debuggerLinesStartAt1: boolean, isServer: boolean = false) {
 		super(debuggerLinesStartAt1, isServer);
-		this.lldb = new LLDBDebugger(this);
+		
+		this.lldb = new LLDB(
+			(str) => this.sendEvent(new OutputEvent(str)),
+			(str) => this.sendEvent(new OutputEvent(str)));
 		this.breakpoints = new Map<string, DebugProtocol.Breakpoint[]>();
 		this.program = null;
 		this.currentline = 0;
@@ -148,14 +133,17 @@ class SwiftDebugSession extends DebugSession {
 	protected launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
 		this.program = args.program;
 		
-		if (args.stopOnEntry) {
-			this.currentline = 0;
-			this.sendResponse(response);
-			this.sendEvent(new StoppedEvent("entry", SwiftDebugSession.MAIN_THREAD_ID));
-		}
-		else {
-			this.continueRequest(response, { threadId: SwiftDebugSession.MAIN_THREAD_ID });
-		}
+		this.lldb.connect(this.program)
+			.then(() => {
+				if (args.stopOnEntry) {
+					this.currentline = 0;
+					this.sendResponse(response);
+					this.sendEvent(new StoppedEvent("entry", SwiftDebugSession.MAIN_THREAD_ID));
+				}
+				else {
+					this.continueRequest(response, { threadId: SwiftDebugSession.MAIN_THREAD_ID });
+				}
+			});
 	}
 
 	protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments): void {
@@ -257,11 +245,14 @@ class SwiftDebugSession extends DebugSession {
 	}
 
 	protected evaluateRequest(response: DebugProtocol.EvaluateResponse, args: DebugProtocol.EvaluateArguments): void {
-		response.body = {
-			result: `evaluate(${args.expression})`,
-			variablesReference: 0
-		};
-		this.sendResponse(response);
+		this.lldb.evaluate(args.expression)
+			.then((result) => {
+				response.body = {
+					result: null,
+					variablesReference: 0
+				};
+				this.sendResponse(response);
+			});
 	}
 }
 
